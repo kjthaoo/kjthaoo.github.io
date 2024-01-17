49f08383ecf4112afd26d66e41802aef0aa22c37
Kjthaoo <kthao43726@gmail.com>
2024-01-17 13:46:46 -0500

    added csce242 folder

---
+++ csce240/main.cc
@@ -0,0 +1,10 @@
+// Copyright 2024 kjthao
+// we will place a comment like the one above in every c++ file we create
+// single line comment
+/* multi-line comment*/
+
+#include<iostream> // pre-processor directive to include the
+                   // input/output stream library
+int main() {
+    std::cout << "hello world!";
+}
