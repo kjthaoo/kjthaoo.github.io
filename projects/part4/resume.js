7b131370bd4b4994ab6cccbf28f4f5176fbd5885
Kjthaoo <kthao43726@gmail.com>
2024-02-21 01:27:39 -0500

    added updated project!!

--- projects/part4/resume.js
+++ projects/part4/resume.js
@@ -1,5 +1,9 @@
-function printResume(KellyThaoResume2024.pdf) {
-    window.open(KellyThaoResume2024.pdf, '_blank');
-}
-
-printResume('KellyThaoResume2024.pdf');
+const myFunction = () => {
+    const winScroll = window.scrollY;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("bar").style.width = `${scrolled}%`;
+  };
+  
+  window.onscroll = myFunction;
+  
