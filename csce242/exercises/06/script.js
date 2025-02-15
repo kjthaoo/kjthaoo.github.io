33cb557987feffdbc3f3bf41ba3b23d7a1de9dd1
Kelly Thao <kjthao@email.sc.edu>
2025-02-14 20:34:32 -0500

    fixed wireframes for a better start

---
+++ csce242/exercises/06/script.js
@@ -0,0 +1,65 @@
+//do stuff when button clicked
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("WOW!");
+    document.getElementById("result").innerHTML="Hi Portia";
+};
+
+//change color
+document.getElementById("btn-color").onclick = () => {
+    const messageP = document.getElementById("message");
+    messageP.innerHTML = "Good bye";
+    messageP.classList.toggle("sad");
+
+}
+
+//happy script
+document.getElementById("btn-happy").onclick = () => {
+    const displayP = document.getElementById("display");
+    displayP.classList.remove("hidden");
+    displayP.classList.add("happy");
+    displayP.innerHTML = "Good Times";
+};
+
+//sad script
+document.getElementById("btn-sad").onclick = () => {
+    const displayP = document.getElementById("display");
+    displayP.classList.remove("hidden");
+    displayP.classList.add("sad");
+    displayP.innerHTML = "Sad TImes";
+};
+
+document.getElementById("btn-clear").onclick = () => {
+    document.getElementById("display").classList.add("hidden");
+};
+
+//key down
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    //const emotion = document.getElementById("txt-emotion").value;
+    document.getElementById("emotional-message").innerHTML = 
+    "You are feeling " + event.currentTarget.value;
+}
+
+//toggling the nav
+document.getElementById("toggle-nav").onclick = () => {
+      document.getElementById("nav-items"),classList.toggle("hide-small");
+};
+
+//color chooser
+document.getElementById("btn-choose-color").onclick = () => {
+    const color = document.getElementById("txt-color").value.toLowerCase().trim();
+    let mood = "Happy"; /*this should be a let, not a const mood*/
+
+    if(color == "blue") {
+        mood = "grumpy";
+    }
+    else if (color == "yellow"){
+        mood = "mellow";
+    }
+    else {
+        mood = "undefined";
+    }
+
+    //document.getElementById("color-message").innerHTML = "You choose " + color; /*somethign and adding it with something else is a k-catnation or whatever its spelt*/
+    document.getElementById("color-message").innerHTML = `You chose ${color}. You are feeling ${mood}.`
+
+};
