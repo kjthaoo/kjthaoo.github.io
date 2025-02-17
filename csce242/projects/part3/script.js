f66921946957e6c1d12714f2f207d79da6b4257b
Kelly Thao <kjthao@email.sc.edu>
2025-02-16 23:04:59 -0500

    updated project 3 index

--- csce242/projects/part3/script.js
+++ csce242/projects/part3/script.js
@@ -2,3 +2,13 @@
 document.getElementById("toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
+
+//go back to the top function
+document.getElementById('back-to-top').addEventListener('click', (e) => {
+    e.preventDefault(); 
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
