aa1327ec4e003b486451668d46ccf5b95fe25271
Kelly Thao <kjthao@email.sc.edu>
2025-02-27 09:33:32 -0500

    added class exercise 6, modal, popup, etc.

---
+++ csce242/exercises/06/js/slideshow.js
@@ -0,0 +1,26 @@
+/*const showSlides = () => {
+    console.log("hi");
+}
+setInterval(showSlides, 5000);
+
+let currentSlides = document.getElementById("slideshow").className;
+*/
+
+/*
+setInterval (()=>{
+    const currentSlide = document.querySelector("#slideshow :not(.hidden)"); /*will be able to read #, by id, etc. bascially selecting something*/
+    /*console.log("current slide");*/
+   /* let nextSlide = currentSlide.nextElementSibling; /*using that top css ad switching it to js*/
+    /*console.log("nextSlide");*/
+
+   /* console.log(nextSlide); /*shows you the image src in console, if the next one is null then do this (below)*/
+
+  /*  if(nextSlide == null){
+        nextSlide = document.querySelector("#slideshow :first-child");
+    }/*reminder to use 'let' instead of 'const' so this would work*/
+
+  /*  currentSlide.classList.add("hidden");
+    nextSlide.classList.remove("hidden");
+},1000);
+
+*/
