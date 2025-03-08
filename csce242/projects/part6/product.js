742588ac2a5b491924afb629b22ecccb660b84b0
Kelly Thao <kjthao@email.sc.edu>
2025-03-07 20:55:07 -0500

    fixed shop.json, doing the json on the shop menu items instead of the product itself

--- csce242/projects/part6/product.js
+++ csce242/projects/part6/product.js
@@ -1,47 +1,3 @@
-const getProducts = async() => {
-    const url = "https://kjthaoo.github.io/csce242/json/product.json";
-
-    try {
-        const response = await fetch(url);
-        return response.json();
-    } catch(error){
-        console.log(error);
-    }
-};
-
-const showProducts = async() => {
-    const products = await getProducts();
-    const productsSection = document.getElementById("products-section");
-    
-    products.forEach((product)=>{
-        const section = document.createElement("section");
-        productsSection.append(section);
-        const h3 = document.createElement("h3");
-        h3.innerHTML = product.name;
-        section.append(h3);
-
-        const p = document.createElement("p");
-        section.append(p);
-        p.innerHTML = `Price: ${product.price}`
-        p.innerHTML = `Description: ${product.description}`
-        p.innerHTML = `Rating: ${product.rating}`
-
-        const ul = document.createElement("ul");
-        section.append(ul);
-
-        //loop through each review
-        product.reviews.forEach((review)=>{
-            const li = document.createElement("li");
-            li.append(review);
-            ul.append(li);
-        });
-
-    });
-};
-
-showProducts();
-
-/*
 document.addEventListener("DOMContentLoaded", () => {
     const urlParams = new URLSearchParams(window.location.search);
     const productName = urlParams.get("product");
@@ -150,7 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("Item added to cart!");
     });
 });
-*/
+
 
 // toggles nav
 const toggleNav = document.getElementById('toggle-nav');
--- csce242/projects/part6/shop.js
+++ csce242/projects/part6/shop.js
@@ -1,3 +1,45 @@
+const getProducts = async() => {
+    const url = "https://kjthaoo.github.io/csce242/json/shop.json";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+};
+
+const showProducts = async() => {
+    const products = await getProducts();
+    const productsSection = document.getElementById("products-section");
+    
+    products.forEach((product)=>{
+        const section = document.createElement("section");
+        productsSection.append(section);
+        const h3 = document.createElement("h3");
+        h3.innerHTML = product.name;
+        section.append(h3);
+
+        const p = document.createElement("p");
+        section.append(p);
+        p.innerHTML = `Price: ${product.price}`
+        p.innerHTML = `Description: ${product.description}`
+        p.innerHTML = `Rating: ${product.rating}`
+
+        const ul = document.createElement("ul");
+        section.append(ul);
+
+        //loop through each review
+        product.reviews.forEach((review)=>{
+            const li = document.createElement("li");
+            li.append(review);
+            ul.append(li);
+        });
+
+    });
+};
+
+showProducts();
 //toggling the nav
 const toggleNav = document.getElementById('toggle-nav');
 const navItems = document.getElementById('nav-items');
